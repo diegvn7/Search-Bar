3224c4b90d549065064f5bb3ba815d6ff4a67d32
diegvn7 <diegovn7@gmail.com>
2025-04-08 19:20:02 -0300

    Update script.js

--- src/js/script.js
+++ src/js/script.js
@@ -1,9 +1,34 @@
 const searchInput = window.document.getElementById('search')
 
 searchInput.addEventListener('input', (event) => {
-    const value = event.target.value
+    const value = formatStringh(event.target.value)
+
+    const items = window.document.querySelectorAll('.item', 'items')
+    const noResults = window.document.getElementById('no_results')
+
+    let hasResults = false
+
+    if (value != '') 
+    items.forEach(item => {
+        const itemTitle = item.querySelector('.item-title').textContent
+
+        if (formatStringh(itemTitle).indexOf(value) !== -1) {
+            item.style.display = 'flex'
+
+            hasResults = 'none'
+        } else {
+             item.style.display = 'none'
+        }
+    })
+
+    if (hasResults) {
+        noResults.style.display = 'none'
+    } else {
+        noResults.style.display = 'block'
+        console.log('gay')
+    }
 })
 
 function formatStringh(value) {
-    return value.toLowerCase().trim()
+    return value.toLowerCase().trim().normalize('NFD').replace(/[\u0300-\u036f]/g, '')
 }
